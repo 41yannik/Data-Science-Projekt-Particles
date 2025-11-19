3553cf4f660adb7e780da36d1146e962784f71a5
22wayan <schmidtwayan@gmail.com>
2025-11-19 13:25:36 +0100

    Initial project structure setup

---
+++ particle_life/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about python_package."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Wayan Schmidt, Yannik Huber, Azad Aygün & Arian Sharifi-Tabar"
+__email__ = "yourname@hs-duesseldorf.de"
+__version__ = "0.1.0"
